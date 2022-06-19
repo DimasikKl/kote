6ae065c3677113a9becd725ee589e21a71f742cc
Kloyp Dmitriy <dimaKl93@yandex.ru>
2022-06-19 21:45:09 +0300

    первый комит

---
+++ kote/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Card from './components/card/card';
+
+const App = (props) => {
+  return(
+    <Card />
+  )
+}
+
+export default App;
---
+++ kote/src/components/card/card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            Карточка
+        </div>
+    )
+}
+
+export default Card;
